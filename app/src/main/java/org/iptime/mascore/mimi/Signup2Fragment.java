64b2a271edec416b9922e6db5eb664f69a410e37
congjeon <wjs982247@gmail.com>
2017-08-17 05:13:05 +0900

    gui

--- app/src/main/java/org/iptime/mascore/mimi/Signup2Fragment.java
+++ app/src/main/java/org/iptime/mascore/mimi/Signup2Fragment.java
@@ -1,11 +1,15 @@
 package org.iptime.mascore.mimi;
 
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.RadioButton;
 import android.widget.TextView;
 
 /**
@@ -13,6 +17,7 @@ import android.widget.TextView;
  */
 public class Signup2Fragment extends Fragment {
 
+    CheckBox chkMale, chkFemale;
 
     public Signup2Fragment() {
         // Required empty public constructor
@@ -23,6 +28,32 @@ public class Signup2Fragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_signup2, container, false);
+
+        chkMale = (CheckBox) view.findViewById(R.id.radioButtonMale);
+        chkFemale = (CheckBox) view.findViewById(R.id.radioButtonFemale);
+
+        chkMale.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked == true) {
+                    chkMale.setTextColor(Color.WHITE);
+                } else {
+                    chkMale.setTextColor(Color.DKGRAY);
+                }
+            }
+        });
+
+        chkFemale.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked == true) {
+                    chkFemale.setTextColor(Color.WHITE);
+                } else {
+                    chkFemale.setTextColor(Color.DKGRAY);
+                }
+            }
+        });
+
         return view;
     }
 
